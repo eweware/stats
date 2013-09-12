8123349fb8476b19a8b787311059074e1a9761d1
Roo deKoo <rk_java@yahoo.com>
2013-09-12 11:40:19 -0700

    First checkin

---
+++ scripts/push-to-dev.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -ex
+rm -f ../src.zip
+zip -r ../src.zip src
+scp -i $OREGON ../src.zip ubuntu@dev.blahgua.com:/home/ubuntu/dev/stats/
+scp -i $OREGON ../pom.xml ubuntu@dev.blahgua.com:/home/ubuntu/dev/stats/
+rm -f ../src.zip
